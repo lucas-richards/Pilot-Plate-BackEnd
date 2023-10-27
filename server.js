b05a916111e3493b8f95b4df4379c166ddc36c4b
lucas richards dev <lucasrichardsdev@gmail.com>
2023-10-27 12:08:48 -0700

    add deployment link

--- server.js
+++ server.js
@@ -41,7 +41,8 @@ const app = express()
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
 app.use(
 	cors({
-		origin: process.env.CLIENT_ORIGIN || `http://localhost:${clientDevPort}`,
+		origin:[ process.env.CLIENT_ORIGIN || `http://localhost:${clientDevPort}`,
+		'https://plate-pilot.onrender.com']
 	})
 )
 
