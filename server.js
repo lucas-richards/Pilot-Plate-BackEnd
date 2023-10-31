6fd920dd439e003ffd475fa09c83122dca14c6c4
lucas richards dev <lucasrichardsdev@gmail.com>
2023-10-31 11:13:21 -0700

    console log to see the error

--- server.js
+++ server.js
@@ -58,13 +58,14 @@ try {
 		
 	},
 	
+	
 });
 	console.log('#############req.query',req.query)
 	
 	const data = await response.json();
 	res.json(data);
 } catch (error) {
-	res.status(500).json({ error: 'An error occurred',error });
+	console.log('this is the yelp api erro',error)
 }
 });
   
