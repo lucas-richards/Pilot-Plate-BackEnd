c0658ffa032cfa3bf171445a3f8393b0bd15a123
lucas richards dev <lucasrichardsdev@gmail.com>
2024-01-19 15:31:51 -0800

    change

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -32,7 +32,7 @@ router.put('/users/:id/add_friend', requireToken, (req, res, next) => {
 	console.log('this is req.body =',req.body)
 	console.log('this is req.params =',req.params)
 	// find the user by its id
-	User.findById(userId).populate('friends')
+	User.findById(userId)
 		.then((user) => {
 			// add the friend to the friends array
 			user.friends.push(friendEmail)
