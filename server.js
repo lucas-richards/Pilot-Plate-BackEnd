d4ad300db31ed74d20e7b39f21401626fe145ac7
lucas richards dev <lucasrichardsdev@gmail.com>
2024-01-23 10:55:01 -0800

    categories to term

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.get('/yelp-data/:loc', async (req, res) => {
 
 try {
 	console.log('API_KEY=',process.env.API_KEY)
-	const response = await fetch(`https://api.yelp.com/v3/businesses/search?location=${req.query.location}&price=${req.query.price}&categories=${req.query.category}&radius=${req.query.radius}&limit=50`, {
+	const response = await fetch(`https://api.yelp.com/v3/businesses/search?location=${req.query.location}&price=${req.query.price}&term=${req.query.category}&radius=${req.query.radius}&limit=50`, {
 		method: 'GET',
 		headers: {
 		'Authorization': process.env.API_KEY,
